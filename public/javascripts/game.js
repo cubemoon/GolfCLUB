05a73ae51b160a90923be0c80915c9bfebf022fb
yanghua <yanghua1127@gmail.com>
2013-11-21 21:57:11 +0800

    js for game pages

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,24 @@
 $(function () {
 
-    var pathname = window.location.pathname;
-
-    if (pathname === "/home") {
-        pathname = "/game/cqticket";
-    }
-
+    initDefaultBehavior();
     
 });
+
+/**
+ * init default behavior
+ * @return {null} 
+ */
+function initDefaultBehavior () {
+    highlightDefaultPlayBtns();
+}
+
+/**
+ * high light default(first) play button
+ * @return {null} 
+ */
+function highlightDefaultPlayBtns () {
+    //play category
+    $("#category button").first().removeClass("btn-default").addClass("btn-primary");
+}
 
 
